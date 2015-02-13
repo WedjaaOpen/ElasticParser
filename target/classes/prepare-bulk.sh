50d8e71a237dcab6f57624d63305dcdbf0572073
Fabio Torchetti <fabio.torchetti@wedjaa.net>
2015-02-13 11:36:35 -0600

    Removed test data and compiled classes.

--- target/classes/prepare-bulk.sh
+++
@@ -1,1 +0,0 @@
-curl http://data.githubarchive.org/2014-08-10-10.json.gz | gzcat | awk '{ sub(/{"created_at":/, "{ \"index\" : { \"_index\" : \"unit\", \"_type\" : \"test\" } }\n{\"created_at\":"); print}' > bulk-insert.json
